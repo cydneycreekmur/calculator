20a86c7b103f4dc2dd7e40a6e65f542c79bd1613
Cydney Creekmur <cac614@scarletmail.rutgers.edu>
2026-02-10 21:20:20 -0500

    first commit

---
+++ simpleCalculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // initialize numbers and sum variables
+    double x, y;
+
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    cout << "The sum is " << x + y << "\n";
+}
