b4f0baded5530039750f74586d77b6043575f247
andre <andrewg511@gmail.com>
2026-02-10 23:42:52 -0500

    Aded power

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 
 using namespace std;
 
-double pi = acos(-1);
-double e = exp(1.0);
+long double pi = acos(-1);
+long double e = exp(1.0);
 // need functions for arithmetic
 
 // addition
-double add(double x, double y) {
+long double add(long double x, long double y) {
     return x + y;
 }
 // subtraction
-double subtract(double x, double y) {
+long double subtract(long double x, long double y) {
     return x - y;
 }
 // mult - Cyd
 // div - Cyd
 // log - Cyd
-// power - Andrew
-// sqrt - Andrew
-// euler
+
+/**
+ * Power function x ^ y
+ * @author Andrew
+ */
+long double power(long double x, long double y){
+    if (y == 1){
+        return x;
+    }
+    return x * power(x, y-1);
+}
+
+ /**
+ * Square Root function x^(1/y)
+ * @author Andrew
+ */
+
+ /**
+ * Euler function (e^x)
+ * @author Andrew
+ */
 
 int main() {
     // initialize numbers and sum variables
@@ -30,5 +49,6 @@ int main() {
     cin >> x;
     cout << "Type another number: ";
     cin >> y;
+    cout << "The number " + to_string(static_cast<double>(x)) + " to the power of " + to_string(static_cast<double>(y)) + " is " + to_string(static_cast<double>(power(x, y))) + "\n";
     cout << "The sum is " << x + y << "\n";
 }
